5deadf60c1dff9eb1e3cfc3e8b32c4b64c11f25a
Jose Raul Ruiz Torres (DevApps) <jose.ruiz@devapps.com.mx>
2025-10-07 15:47:19 -0600

    feat: definir modelo de datos y tabla para Producto

---
+++ backend/src/main/java/com/practica_fullstack_devapps/backend/entity/Producto.java
@@ -0,0 +1,29 @@
+package com.practica_fullstack_devapps.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "productos")
+@Data
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nombre;
+
+    private String marca;
+
+    private String categoria;
+
+    @Column(nullable = false)
+    private Double precio;
+
+    @Column(nullable = false)
+    private Integer existencias;
+
+    private boolean activo;
+}
